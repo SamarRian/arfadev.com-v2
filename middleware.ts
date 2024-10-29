3b670d9f9e414da30048a29eb987719c257ca984
Umar Saleem <umersaleem50@gmail.com>
2024-10-29 21:48:46 +0500

    fixed favicon issue

--- middleware.ts
+++ middleware.ts
@@ -1,50 +1,44 @@
-import { match } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
 
 import { NextRequest, NextResponse } from "next/server";
 
- 
+let locales = ["en", "de"];
+let defaultLocale = "en-US";
 
-let locales = ['en', 'de']
-let defaultLocale = 'en-US'
- 
+export const getLocale = (request: NextRequest) => {
+  let headers = {
+    "accept-language":
+      request.headers.get("accept-language") || "en-US,en;q=0.5",
+  };
+  let languages = new Negotiator({ headers }).languages();
 
+  return match(languages, locales, defaultLocale); // -> 'en-US'
+};
 
-export const getLocale = (request:NextRequest) => {
-    let headers = { 'accept-language': request.headers.get("accept-language") || 'en-US,en;q=0.5' }
-let languages = new Negotiator({ headers }).languages()
-
-
- return match(languages, locales, defaultLocale) // -> 'en-US'
-
-}
-
-
- 
- 
-export function middleware(request:NextRequest) {
+export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl
+  const { pathname } = request.nextUrl;
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  )
- 
-  if (pathnameHasLocale) return
- 
+  );
+
+  if (pathnameHasLocale) return;
+
   // Redirect if there is no locale
   const locale = getLocale(request);
-  request.nextUrl.pathname = `/${locale}${pathname}`
+  request.nextUrl.pathname = `/${locale}${pathname}`;
   // e.g. incoming request is /products
   // The new URL is now /en-US/products
-  return NextResponse.redirect(request.nextUrl)
+  return NextResponse.redirect(request.nextUrl);
 }
- 
+
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
     // '/((?!_next).*)',
-    '/((?!_next|studio|favicon|public|assets).*)',
+    "/((?!_next|studio|favicon|public|assets|icon).*)",
     // Optional: only run on root (/) URL
     // '/'
   ],
-}
+};
